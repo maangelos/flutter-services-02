216f08d5946bfd54ff01f9a8395fa307e7693166
Marianna Alves <51267286+maangelos@users.noreply.github.com>
2020-02-28 08:50:03 -0300

    Update url.dart

--- lib/url/url.dart
+++ lib/url/url.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-const url = "http://192.112.1.4:5000/";
+const url = "http:/ip:5000/";
 
 class URL {
   static Future getUsuarios() {
     return http.get(url);
   }
 }
+
+//conseguir ip via ipconfig e add no lugar de ip
